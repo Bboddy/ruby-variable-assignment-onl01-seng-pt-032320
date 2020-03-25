a552f064292b79cf6202add54da4f843cbfbb478
puckish-system-8282 <bryce.boddy@gmail.com>
2020-03-25 21:52:17 +0000

    Automatically backed up by Learn

--- variable.rb
+++ variable.rb
@@ -1,3 +1,7 @@
  # Define a local variable called 'greeting',
+<<<<<<< HEAD
  # give it the value of the string "Hello World"
  greeting = "Hello World"
+=======
+ # give it the value of the string "Hello 
+>>>>>>> cba2c6ac81aa9909cddf26cbbd6c8a483515791c
